8d9a7d5ebc5031ab89f71f504f59439c9818b0db
João Vitor <joaovitor.felipesantos@gmail.com>
2020-06-19 08:46:51 -0300

    Implementa funções de routing entre as páginas (front-end)

--- tempo-telecom-front/front-angular/src/app/app-routing.module.ts
+++ tempo-telecom-front/front-angular/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ClienteComponent } from './pages/cliente/cliente.component';
+import { ProdutoComponent } from './pages/produto/produto.component';
+import { PedidoComponent } from './pages/pedido/pedido.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  { path: '', component: HomeComponent },
+  { path: 'clientes', component: ClienteComponent },
+  { path: 'produtos', component: ProdutoComponent },
+  { path: 'pedidos', component: PedidoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+}
--- tempo-telecom-front/front-angular/src/app/app.module.ts
+++ tempo-telecom-front/front-angular/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopMenuComponent } from './components/top-menu/top.menu.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ClienteComponent } from './pages/cliente/cliente.component';
+import { PedidoComponent } from './pages/pedido/pedido.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopMenuComponent,
+    HomeComponent,
+    ClienteComponent,
+    PedidoComponent 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
